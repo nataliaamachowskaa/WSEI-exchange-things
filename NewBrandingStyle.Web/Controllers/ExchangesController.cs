e07cda3e5625ab18250742d78478d71d9b60a382
nataliaamachowskaa <noelee94@gmail.com>
2020-10-20 20:52:47 +0200

    Dodaj pliki projektów.

---
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show ()
+
+        {
+            return View();
+        }
+
+    }
+}
